97926971ba03ffe23c45d7fcb46ffe2b3ca0899c
Mango <evan940415@gmail.com>
2022-05-08 14:02:05 +0800

    change dir of func

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
 import os
-
-from discord import Intents
 from discord.ext import commands
 
 import utils.func as utils
 
 config = utils.jload(file='./config', mode='r', encoding='utf8')
 
-def getIntents(items):
-    _map = {'all': Intents.all(), 'default': Intents.default(), 'none': Intents.none()}
-    intents = _map.get(items[0])
-    if len(items) == 1:
-        return intents
-    _dict = intents.__dict__
-    for i in items:
-        _dict[i] = True
-    return intents
-
 bot = commands.Bot(
     command_prefix = config['commands.prefix'],
-    Intents = getIntents(config['commands.intents'])
+    Intents = utils.getIntents(config['commands.intents'])
     )
 
 @bot.event
--- utils/func.py
+++ utils/func.py
@@ -1,6 +1,17 @@
 import json
+from discord import Intents
 
 def jload(file, mode='r', *args, **kwargs):
     file = file if file[-5:] == '.json' else file + '.json'
     with open(file, mode, *args, **kwargs) as file:
         return json.load(file)
+
+def getIntents(items):
+    _map = {'all': Intents.all(), 'default': Intents.default(), 'none': Intents.none()}
+    intents = _map.get(items[0])
+    if len(items) == 1:
+        return intents
+    _dict = intents.__dict__
+    for i in items:
+        _dict[i] = True
+    return intents
